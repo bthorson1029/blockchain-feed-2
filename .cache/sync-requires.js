8946d5a8cd1316f3a2108af4ea2abd61f20616f9
Bryce <brycethorson.design@gmail.com>
2023-12-02 15:00:38 -0500

    adding gatsby dependencies

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Bryce\\code\\blockchain-feed-2\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-tsx": preferDefault(require("C:\\Users\\Bryce\\code\\blockchain-feed-2\\src\\pages\\404.tsx")),
+  "component---src-pages-index-tsx": preferDefault(require("C:\\Users\\Bryce\\code\\blockchain-feed-2\\src\\pages\\index.tsx"))
+}
+
